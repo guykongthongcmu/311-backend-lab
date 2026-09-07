f3bcb6275bb5cb68136c939b0ad0a4c773a9ceeb
672115045 Virawit Kongthong <guykongthongcmu@gmail.com>
2026-09-07 15:07:28 +0700

    lab7-Q4.9

--- src/main/java/se331/lab7/Application.java
+++ src/main/java/se331/lab7/Application.java
@@ -18,8 +18,9 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/events").allowedOrigins("http://localhost:5173");
-                registry.addMapping("/events/*").allowedOrigins("http://localhost:5173");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .exposedHeaders("x-total-count");
             }
         };
     }
--- src/main/java/se331/lab7/controller/EventController.java
+++ src/main/java/se331/lab7/controller/EventController.java
@@ -1,6 +1,7 @@
 package se331.lab7.controller;
 
 import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -99,13 +100,15 @@ public class EventController {
         page = page == null ? 1 : page;
         Integer firstIndex = (page - 1) * perPage;
         List<Event> output = new ArrayList<>();
+        HttpHeaders responseHeader = new HttpHeaders();
+        responseHeader.set("x-total-count", String.valueOf(eventList.size()));
         try {
             for (int i = firstIndex; i < firstIndex + perPage; i++) {
                 output.add(eventList.get(i));
             }
-            return ResponseEntity.ok(output);
+            return ResponseEntity.ok().headers(responseHeader).body(output);
         } catch (IndexOutOfBoundsException ex) {
-            return ResponseEntity.ok(output);
+            return ResponseEntity.ok().headers(responseHeader).body(output);
         }
     }
 
