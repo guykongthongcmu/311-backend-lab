f5d236d1869bbb493fe0f0ec910fd10e7704cb55
672115045 Virawit Kongthong <guykongthongcmu@gmail.com>
2026-09-07 13:45:45 +0700

    lab7-Q2.7

---
+++ src/main/java/se331/lab7/entity/Event.java
@@ -0,0 +1,18 @@
+package se331.lab7.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Event {
+    Long id;
+    String category;
+    String title;
+    String description;
+    String location;
+    String date;
+    String time;
+    Boolean petsAllowed;
+    String organizer;
+}
