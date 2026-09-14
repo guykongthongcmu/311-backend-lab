f8531d6005ebcac41b5de20603ebdeea0355c3ff
672115045 Virawit Kongthong <guykongthongcmu@gmail.com>
2026-09-14 13:28:34 +0700

    8-Q5.4

--- src/main/java/se331/lab7/controller/EventController.java
+++ src/main/java/se331/lab7/controller/EventController.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import se331.lab7.entity.Event;
 
@@ -61,5 +58,11 @@ public class EventController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
         }
+    }
+
+    @PostMapping("/events/")
+    public ResponseEntity<?> addEvent(@RequestBody Event event) {
+        Event output = eventService.save(event);
+        return ResponseEntity.ok(output);
     }
 }
